05bdd7fbb0255c30138ca3e8ff149a467aed0a04
ekarCumAliORUC <105822087+ekarCumAliORUC@users.noreply.github.com>
2024-09-08 12:08:26 +0300

    Palendrome

--- myhomework5.dart
+++ myhomework5.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> args) {
   /*
   Palindrome Checker
@@ -5,4 +7,29 @@ void main(List<String> args) {
   The program should ignore case and spaces. 
   Use string manipulation methods to reverse the string and compare it with the original.
   */
+
+  print("Please enter the sentence to check whether palendrome or not");
+  String sentence = (stdin.readLineSync()!).toLowerCase();
+  sentence = sentence.replaceAll(' ', '');
+  palendrome(sentence);
+}
+
+void palendrome(String sentence) {
+  String reversedSentence = sentence.split("").reversed.join();
+  print(reversedSentence);
+
+  List sentenceList = sentence.trim().split("");
+  List reversedSentenceList = reversedSentence.trim().split("");
+  print(sentenceList);
+  print(reversedSentenceList);
+  int count = 0;
+  for (var i = 0; i < sentenceList.length; i++) {
+    if (sentenceList[i] == reversedSentenceList[i]) count++;
+  }
+  print(count);
+  print(sentenceList.length);
+  if (count == (sentenceList.length))
+    print("$sentence has a palendrome");
+  else
+    print("$sentence has not a palendrome");
 }
